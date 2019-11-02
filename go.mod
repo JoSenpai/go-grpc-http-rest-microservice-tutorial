3df75bef6485c7dd915c10a7d2a66f57d31fd5a9
Jonathan Yong <jonathan.yong@papercut.com>
2019-11-02 19:27:44 +1100

    First commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/josenpai/go-grpc-http-rest-microservice-tutorial
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	google.golang.org/grpc v1.24.0
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0
+)
---
+++ pkg/protocol/grpc/server.go
@@ -0,0 +1,36 @@
+package grpc
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+
+)
+
+// RunServer runs gRPC service to publish ToDo service
+func RunServer(ctx context.Context, v1API v1.ToDoServiceServer, port string) error {
+
+	listen, err := net.Listen("tcp", ":" + port)
+	if err != nil {
+		return err
+	}
+
+	//register service
+	server := grpc.NewServer()
+	v1.RegisterToDoServiceServer(server, v1API)
+	
+	//graceful shutdown
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			// sig is ^C, handle it
+			log.Println("shutting down gRPC server...")
+			server.
+		}
+	}
+}
